28adda2f01f01bec8235b47a7058912d5bb318c6
MystIsle <pelhav@gmail.com>
2026-01-07 09:01:24 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -6,12 +6,36 @@ using namespace std;
 
 long long solution(long long n) {
     
-    double sq = sqrt(n);
-    long long sqr = (long long)sq;
+//     double sq = sqrt(n);
+//     long long sqr = (long long)sq;
     
-    if(sqr * sqr == n)
+//     if(sqr * sqr == n)
+//     {
+//         return (sqr + 1) * (sqr + 1) ;
+//     }
+    
+//     return -1;
+    
+    long long left = 1;
+    long long right = 50000000000000;
+    
+    while(left <= right)
     {
-        return (sqr + 1) * (sqr + 1) ;
+        long long mid = (left + right) / 2;
+        
+        unsigned long long square = (unsigned long long) mid * mid;
+        if(square == n)
+        {
+            return (mid + 1) * (mid + 1);
+        }
+        else if(square < n)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid -1;
+        }
     }
     
     return -1;
